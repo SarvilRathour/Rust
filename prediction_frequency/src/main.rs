56e23824e75d47a17af7f848afcb9fb75e00ca2c
Sarvil Rathour <sarvilrathour83@gmail.com>
2025-04-27 09:48:00 +0530

    finally done

--- prediction_frequency/src/main.rs
+++ prediction_frequency/src/main.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read};
 use lopdf::Document;
 use regex::Regex;
+use std::io;
 fn frequency(m:Vec<&str>)->HashMap<String, u32>{
     let mut frequency: HashMap<String, u32> = HashMap::new();
     let re=Regex::new(r"[^a-zA-Z\s]").expect("it is not working");
@@ -14,17 +15,31 @@ fn to_get_largest(largest:HashMap<String,u32>)->String{
         let mut max_count=0;
         let mut max_key=String::new();
         for(k,v) in largest{
-            println!("{k}-----{v}");
+            // println!("{k}-----{v}");
             if v>max_count{
                 max_count=v;
                 max_key=k;
             }
-            
-            println!("The max count is {}",max_count);
         }
-        println!("The max count is {}",max_count);
+        // println!("The max count is {}",max_count);
        max_key.to_string()
 }
+fn ordered(order:HashMap<String,u32>)->Vec<(String,u32)>{
+    let mut items:Vec<(String,u32)>=order.into_iter().collect();
+    items.sort_by(|a,b|b.1.cmp(&a.1));
+    items
+}
+fn prediction(o:Vec<(String,u32)>,n:i32,i:String){
+    let db:Vec<String>=o.iter()
+    .take(n.try_into().unwrap())
+    .map(|(s,_)|s.clone())
+    .collect();
+    let predicted=db.join(" ");
+    let result_final=format!("{} {}",i.trim_end(),predicted);
+    println!("{}",result_final);
+
+}
+
 fn main() {
     let file="src/test.pdf";
     let mut texts: Vec<String>=Vec::new();
@@ -43,6 +58,17 @@ Err(err) =>println!("There was a error {}",err),
     let combined=texts.join(" ").to_lowercase();
     let words:Vec<&str>=combined.split_whitespace().collect();
     let store=frequency(words);
-    let largest=to_get_largest(store);
-    println!("The largest is---{}",largest);
+    let largest=to_get_largest(store.clone());
+    let ordered=ordered(store);
+    // println!("The largest is---{}",largest);
+    let mut input=String::new();
+    println!("Please enter a line you want to filled by our prediction_frequency(crime and punsihment is being used for this):");
+    io::stdin().read_line(&mut input).expect("failed to take the user input");
+    println!("Please enter the number of words you want us to predict:");
+    let mut numbers=String::new();
+    io::stdin().read_line( &mut numbers).expect("failed to load the line for taking length input");
+    let number:i32=numbers.trim().parse().unwrap();
+    prediction(ordered,number,input);
+    // let input_answer=format!("{} {}",input.trim_end(),largest);
+    // println!("Our answer by prediction frequency-{}",input_answer);
 }
